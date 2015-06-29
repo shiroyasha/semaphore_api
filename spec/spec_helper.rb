8d6cf2f21ef1387443235384650fc07c2b4ab159
Igor Sarcevic <igisar@gmail.com>
2015-06-29 22:53:06 +0200

    Add VCR for API testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'semaphore_api'
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "semaphore_api"
+require "byebug"
+require "vcr"
+require "webmock"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = false
+  config.default_cassette_options = { :record => :once }
+  config.configure_rspec_metadata!
+end
