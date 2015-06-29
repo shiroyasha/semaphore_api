063de682c959048bb1ba682e8a82a8aa3e9d814d
Igor Sarcevic <igisar@gmail.com>
2015-06-29 22:11:45 +0200

    Add remaining API actions

--- lib/semaphore_api/client/builds.rb
+++ lib/semaphore_api/client/builds.rb
@@ -10,6 +10,14 @@ module SemaphoreApi
         get "/projects/#{project_hash_id}/#{branch_id}/builds/#{build_number}/log"
       end
 
+      def launch_build(project_hash_id, branch_id, options = {})
+        post "/projects/#{project_hash_id}/#{branch_id}/build"
+      end
+
+      def stop_build(project_hash_id, branch_id, build_number, options = {})
+        post "/projects/#{project_hash_id}/#{branch_id}/builds/#{build_number}/stop"
+      end
+
     end
   end
 end
--- lib/semaphore_api/client/deploys.rb
+++ lib/semaphore_api/client/deploys.rb
@@ -10,6 +10,14 @@ module SemaphoreApi
         get "/projects/#{project_hash_id}/servers/#{server_id}/deploys/#{deploy_number}/log"
       end
 
+      def launch_deploy(project_hash_id, branch_id, build_number, server_id, depoy_number, options = {})
+        post "/projects/#{project_hash_id}/#{branch_id}/builds/#{build_number}/deploy/#{server_id}", options
+      end
+
+      def stop_deploy(project_hash_id, branch_id, deploy_number, options = {})
+        post "/projects/#{project_hash_id}/#{branch_id}/deploys/#{deploy_number}/stop", options
+      end
+
     end
   end
 end
